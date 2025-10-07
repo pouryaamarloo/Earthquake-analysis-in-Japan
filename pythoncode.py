30b13f35071935b0dd2dd350190470deb740cf39
sepideh raeisi <ririsepideh@gmail.com>
2025-10-08 03:24:50 +0330

    read cvs,to datetime,to float ,re_category

--- pythoncode.py
+++ pythoncode.py
@@ -6,4 +6,22 @@ print(df.shape)
 print(df.columns)
 #تبدیل ستون تاریخ به datetime:
 df["time"]=pd.to_datetime(df["time"],errors="coerce")
-digit_column_list=["latitude" ,"longitude","depth","mag","nst","gap","dmin","rms",]
+
+df["year"]=df["time"].dt.year
+df["mounth"]=df["time"].dt.month
+#تبدیل ستون های عددی به float:
+digit_column_list=["latitude" ,"longitude","depth","mag","nst","gap","dmin","rms","horizontalError","depthError","magError","magNst"]
+for i in digit_column_list:
+    if i in df.columns:
+        df[i]=pd.to.numeric(df[i],errors='coerce')
+df=df.dropna()
+
+#تابع شدت زلزله
+def re_category(c):
+    if c<4:
+        return "zaeif"
+    elif 4<c<6:
+        return "motevaset"
+    elif 6<c:
+        return "shadid"
+    
