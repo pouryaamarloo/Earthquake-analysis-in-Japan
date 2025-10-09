2ac8eeb34fff66de69c8bc153ca0487269922f71
SamanehMahdavi <Samaneh.mahdavi.ie99@gmail.com>
2025-10-09 18:27:30 +0330

    SQL Init Script Added

---
+++ database/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE earthquakes (
+    id SERIAL PRIMARY KEY,
+    time TIMESTAMP NOT NULL,
+    longitude FLOAT,
+    latitude FLOAT,
+    depth FLOAT,
+    magnitude FLOAT,
+    region VARCHAR(255),
+    source VARCHAR(50)
+);
+CREATE INDEX idx_time ON earthquakes(time);
+CREATE INDEX idx_region ON earthquakes(region);
+CREATE INDEX idx_magnitude ON earthquakes(magnitude);
