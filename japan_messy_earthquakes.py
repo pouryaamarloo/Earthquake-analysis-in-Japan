44df65833e8ecf107c52146f6296571f82872766
Kimia <k8564imia@gmail.com>
2025-10-09 13:39:01 +0330

    ye sery eshtebeh dasht

--- japan_messy_earthquakes.py
+++ japan_messy_earthquakes.py
@@ -2,43 +2,47 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 #خواندن فایل csv 
-data_f = pd.read_csv("japan_messy_earthquakes.csv")
-print(data_f.head())
+data_f = pd.read_csv("JAPAN_DATASET.csv")
+print(data_f.shape)
+print(data_f.dtypes)
 #تبدیل سنون ها
-data_f['time'] = pd.to_datetime(data_f['time'], errors='coerce') #time -> datetime
-data_f['latitude'] = pd.to_numeric(data_f['latitude'], errors='coerce')  #latitude -> float
-data_f['longitude'] = pd.to_numeric(data_f['longitude'], errors='coerce')  #longitude -> float
-data_f['depth_m'] = pd.to_numeric(data_f['depth_m'], errors='coerce')  #depth_m -> float
-data_f['mag'] = pd.to_numeric(data_f['mag'], errors='coerce')  #mag -> float
+data_f['time'] = pd.to_datetime(data_f['time'] , errors='coerce') #time -> datetime
+data_f['latitude'] = pd.to_numeric(data_f['latitude'] , errors='coerce')  #latitude -> float
+data_f['longitude'] = pd.to_numeric(data_f['longitude'] , errors='coerce')  #longitude -> float
+data_f['depth_m'] = pd.to_numeric(data_f['depth_m'] , errors='coerce')  #depth_m -> float
+data_f['mag'] = pd.to_numeric(data_f['mag'] , errors='coerce')  #mag -> float
 
 #بررسی داده های گمشده
 #تعداد داده های گمشده در هر ستون
 print(data_f.isna().sum())
-#ردیف های عمق و mag دارای مقادیر غیر معقول و خارج از محدوده دارن
-print(data_f[(data_f['depth_m']<0) | (data_f['depth_m']>1000)])
-print(data_f[(data_f['mag']<0) | (data_f['mag']>10)])
 #ردیف های گمشده را باید حذف کنیم که اشتباه نشه
 data_f = data_f.dropna(subset=['latitude' , 'longitude' , 'mag'])
-#برای ردیف عمق غیر منطقی
-data_f.loc[(data_f['depth_m']<0) | (data_f['depth_m']>1000) , 'depth_m'] = np.nan
-data_f = data_f.sort_values('time').reset_index(drop=True)
-#فیلتر کردن بر اساس اندازه و عمق
-#زلزه های بیشتر از 5.0
-quakes = data_f[data_f['mag']>5.0]
-#زلزله کم عمق
-shallow_q = data_f[data_f['depth_m']<70]
-#گروه بندی کردن بر اساس مکان و ماه
-#گروه بندی بر اساس محل وقوع زلزله
-mag_place = data_f.groupby('place')['mag'].mean()
 #زلزله در هر ماه
 data_f['month'] = data_f['time'].dt.month
-quakes_per_month = data_f.groupby('month').size()
-data_f[data_f['quality_flag'].notna()]
+#ساخت ستون category
+def category(i) : #i=شدت زلزله 
+    if i < 4 :
+        return "Week"
+    elif 4<= i <= 6 :
+        return "Moderate"
+    elif i > 6 :
+        return "Strong"
 
-#اندازه زلزله بر اساس تاریخ
-plt.figure(figsize=(10,5))
-plt.plot(data_f['time'] , data_f['mag'] , marker = 'o' , linestyle = 'None')
-plt.xlabel('Date')
-plt.ylabel('Magnitude')
-plt.title('*Earthquake Magnitude Over Time*')
-plt.show()
+data_f['Category'] = data_f['mag'].apply(category)
+
+data_f['region'] = data_f['place'].astype(str).apply(lambda x : x.split(',')[0].strip())
+#گروهبندی بر اساس ماه و category
+group_1 = data_f.groupby(['Month' , 'Category']).agg(
+        count = ('mag' , 'size') ,
+        meangin_mag = ('mag' , 'mean') 
+    )
+print(group_1)
+#گروهبندی بر اساس منطقه
+group_2 = data_f.groupby('region').agg(
+    quake_count = ('mag' , 'size') ,
+    meangin_mag = ('mag' , 'mean') ,
+    meangin_depth = ('depth_m' , 'mean') ,
+    max_mag = ('mag' , 'max') ,
+    max_depth = ('depth_m' , 'max')
+    )
+print(group_2)
