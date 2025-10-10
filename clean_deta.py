f991a81040c2917d341cb30556535fb05ba6d4bd
pourya <pouryaamarloo162@gmail.com>
2025-10-10 12:06:17 +0330

    edit

--- clean_deta.py
+++ clean_deta.py
@@ -4,7 +4,7 @@ import pandas as pd
 df=pd.read_csv("JAPAN_GEOFON.csv", header=None)
 
 #cut data(,)
-print(df.columns)
+print(df.shape)
 #colums name
 # df.columns = ['Magnitude' , 'location' , 'data' , 'time' , 'Longitude' , 'Latitude' , 'Depth']
 df = df[[0, 1, 2, 3, 4, 5, 6]].rename(columns={0:'Magnitude', 1:'location', 2:'Date',3:'Time',4:'Longitude',5:'Latitude',6:'Depth'})
@@ -27,7 +27,8 @@ df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], errors='coerce')
 
 #
 df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], errors='coerce')
-
+df["Month"] = df['Datetime'].dt.month
+# df["Category"] = df['Magnitude'].apply(lambda x )
 #
 df = df.sort_values(by='Datetime')
 
@@ -38,4 +39,4 @@ df = df.dropna(subset=['Datetime'])
 df.to_csv("JAPAN_GEOFON_cleaned.csv", index=False)
 
 print(" داده‌ها با موفقیت تمیز و در فایل JAPAN_GEOFON_cleaned.csv ذخیره شدند!")
-print(df.head())
+print(df)
