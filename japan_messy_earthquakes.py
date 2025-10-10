1afc70e9bf56a8fa29959b5d9955232fa62d4782
Kimia <k8564imia@gmail.com>
2025-10-10 19:03:51 +0330

    numpy addad

--- japan_messy_earthquakes.py
+++ japan_messy_earthquakes.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 #خواندن فایل csv 
 data_f = pd.read_csv("JAPAN_DATASET.csv")
+#نمایش تعداد ردیف ها و ستون ها
 print(data_f.shape)
+#نمایش نوع داده هر ستون
 print(data_f.dtypes)
-#تبدیل سنون ها
+#تبدیل نوع داده ها
 data_f['time'] = pd.to_datetime(data_f['time'] , errors='coerce') #time -> datetime
 data_f['latitude'] = pd.to_numeric(data_f['latitude'] , errors='coerce')  #latitude -> float
 data_f['longitude'] = pd.to_numeric(data_f['longitude'] , errors='coerce')  #longitude -> float
@@ -13,14 +16,38 @@ data_f['depth_m'] = pd.to_numeric(data_f['depth_m'] , errors='coerce')  #depth_m
 data_f['mag'] = pd.to_numeric(data_f['mag'] , errors='coerce')  #mag -> float
 
 #بررسی داده های گمشده
-#تعداد داده های گمشده در هر ستون
-#ردیف های گمشده را باید حذف کنیم که اشتباه نشه
+#حذف ردیف هایی که مقدار mag / longitude / latitude ندارند!
 data_f = data_f.dropna(subset=['latitude' , 'longitude' , 'mag'])
+#جایگزین کردن میانگین  عمق به جای مقادیر گمشده
 data_f['depth_m'] = data_f['depth_m'].fillna(data_f['depth_m'].mean())
 print(data_f.isna().sum())
+
+#مختصات شهر توکیو
+#از فایل csv متخصات برداشته شده!
+tokyo_latitude = 35.6895
+tokyo_longitude = 139.6917
+#محاسبه فاصله اقلیدسی هر زلزله تا توکیو
+#فرمول : dist = np.sqrt((x2 - x1)2 + (y2 - y1)2) {--> x1,y1 طول و عرض جغرافیایی توکیو هستند}
+data_f['distance_to_tokyo'] = np.sqrt(
+    (data_f['latitude'] - tokyo_latitude)**2 +
+    (data_f['longitude'] - tokyo_longitude)**2
+    )
+#محاسبات آماری و ریاضی
+print("Mean of distance :" , np.mean(data_f['distance_to_tokyo']))
+print("Standard deviation of distance :" , np.std(data_f['distance_to_tokyo']))
+print("Variance of distance :" , np.var(data_f['distance_to_tokyo']))
+print("Percentile of distance :" , np.percentile(data_f['distance_to_tokyo'] , ))  #صدک چندم؟
+                                                 
+#ذخیره ستون جدید در فایل(ستون فاصله تا توکیو)
+data_f.to_csv("JAPAN_DATASET.csv" , index = False)
+
+print("JAPAN_DATASET file saved with new changes!")
+
 #زلزله در هر ماه
 data_f['Month'] = data_f['time'].dt.month
+
 #ساخت ستون category
+#دسته بندی براساس mag
 def category(i) : #i=شدت زلزله 
     if i < 4 :
         return "Weak"
@@ -48,6 +75,7 @@ group_2 = data_f.groupby('region').agg(
     )
 print(group_2)
 
+#رسم نمودار میله ای
 plt.figure(figsize = (12 , 6))
 plt.bar(group_2.index , group_2['quake_count'])
 plt.xticks(rotation = 45 , ha = 'right')
